8173d64a3354faa60a913620382b5828ef68c9a9
Lucas Eduardo Gomes <lucasgomes78@gmail.com>
2019-08-06 23:18:19 -0300

    Registro de Usuario

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,9 @@
 const express = require('express');
-
 const routes = express.Router();
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
 
-routes.get('/', (req, res) => {
-  return res.send(req.query.name);
-});
-
-routes.post('/devs', (req, res) => {
-  return res.json({message: "Ok"})
-})
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect("mongodb+srv://lucas:77562@devle-database-f8sfa.mongodb.net/devle?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
