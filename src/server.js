f552e908fb68e9499ed23e0431c8a8e1e4688e55
Lucas Eduardo Gomes <lucasgomes78@gmail.com>
2019-08-06 23:45:58 -0300

    Dislike e Lista de usuarios

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +10,7 @@ mongoose.connect("mongodb+srv://lucas:77562@devle-database-f8sfa.mongodb.net/dev
   useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
